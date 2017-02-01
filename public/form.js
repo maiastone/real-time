c0a3834ff4c516853c826172c9a1b12dc08fa9b5
Maia Stone <maiadov@gmail.com>
2017-01-31 23:20:19 -0700

    Logs form data object

--- public/form.js
+++ public/form.js
@@ -4,9 +4,26 @@ $(document).ready(function() {
     e.preventDefault();
     console.log('form submitted!');
     const formData = {
-      name: $('#name').val()
+      name: $('#name').val(),
+      selections: [
+        {
+          id: 1,
+          text: $('#A').val()
+        },
+        {
+          id: 2,
+          text: $('#B').val()
+        },
+        {
+          id: 3,
+          text: $('#C').val()
+        },
+        {
+          id: 4,
+          text: $('#D').val()
+        }
+      ]
     }
-    console.log(formData);
-  })
-
+      console.log(formData);
+    })
 })
