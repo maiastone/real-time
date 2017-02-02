34cc09b7a0e1a24b5a0162687aa023393369d068
Maia Stone <maiadov@gmail.com>
2017-02-02 15:47:31 -0700

    Refactor renderSurvey

--- public/form.js
+++ public/form.js
@@ -5,7 +5,7 @@ const buttons = document.querySelectorAll('.select buttons');
 
 $(document).ready(function() {
   let surveyID = getParameterByName('surveyID');
-  fetchRenderSurvey(surveyID);
+  fetchSurvey(surveyID);
   $('.complete-survey').on('click', 'button', function(e) {
     e.preventDefault();
     socket.send('voteCast', this.innerText);
@@ -51,52 +51,40 @@ function getParameterByName(name, url) {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-
-  function fetchRenderSurvey(surveyID) {
-    $.get(`/api/survey/${surveyID}`, function(data) {
-        if (surveyID === null) {
-          console.log('There is no survey')
+function fetchSurvey(surveyID) {
+  $.get(`/api/survey/${surveyID}`, function(data) {
+      if (surveyID === null) {
+        console.log('There is no survey')
+      } else {
+        $('.form').hide();
+        if (localStorage.getItem('id_token')) {
+          renderSurvey(data);
         } else {
-          $('.form').hide();
-          if (localStorage.getItem('id_token')) {
-            let name = data.formData.survey.name;
-            let selection1 = data.formData.survey.selections[0].text;
-            let selection2 = data.formData.survey.selections[1].text;
-            let selection3 = data.formData.survey.selections[2].text;
-            let selection4 = data.formData.survey.selections[3].text;
-            $('.question').append(`
-              <h2 class="form-name">${name}</h2>`)
-            $('.selection-1').append(`<div class='selection-container'>
-              <button
-                name='answer'
-                class='select'
-                value='selection1'>A
-              </button>
-              <h3 class='selection'>${selection1}</h3></div>`)
-            $('.selection-2').append(`<div class='selection-container'>
-              <button
-                name='answer'
-                class='select'
-                value='selection2'>B
-              </button>
-              <h3 class='selection'>${selection2}</h3></div>`)
-            $('.selection-3').append(`<div class='selection-container'>
-              <button
-                name='answer'
-                class='select'
-                value='selection3'>C
-              </button>
-              <h3 class='selection'>${selection3}</h3></div>`)
-            $('.selection-4').append(`<div class='selection-container'>
-              <button
-                name='answer'
-                class='select'
-                value='selection4'>D
-              </button>
-              <h3 class='selection'>${selection4}</h3></div>`);
-          } else {
-            $('#btn-login').show();
-          }
+          $('#btn-login').show();
         }
-      })
-    }
+      }
+    })
+  }
+
+function appendSelection(className, value, buttonText, selectionText) {
+  $(className).append(`<div class='selection-container'>
+    <button
+      name='answer'
+      class='select'
+      value=${value}>${buttonText}
+    </button>
+    <h3 class='selection'>${selectionText}</h3></div>`)
+};
+
+function renderSurvey(data) {
+  let name = data.formData.survey.name;
+  let selection1 = data.formData.survey.selections[0].text;
+  let selection2 = data.formData.survey.selections[1].text;
+  let selection3 = data.formData.survey.selections[2].text;
+  let selection4 = data.formData.survey.selections[3].text;
+  $('.question').append(`<h2 class='form-name'>${name}</h2>`)
+  appendSelection('.selection-1', 'selection1', 'A', selection1)
+  appendSelection('.selection-2', 'selection2', 'B', selection2)
+  appendSelection('.selection-3', 'selection3', 'C', selection3)
+  appendSelection('.selection-4', 'selection4', 'D', selection4)
+};
