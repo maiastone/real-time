6dfbf8cf672f5e5d243e0ec9dbbb44c92b3885eb
Maia Stone <maiadov@gmail.com>
2017-02-02 00:59:30 -0700

    Renders styled survey to DOM

--- public/form.js
+++ public/form.js
@@ -1,20 +1,57 @@
 $(document).ready(function() {
-
   let surveyID = getParameterByName('surveyID');
-
-  fetchSurvey(surveyID);
-  renderSurvey(surveyID);
+  fetchRenderSurvey(surveyID);
 
 });
 
-  function fetchSurvey(surveyID) {
+  function fetchRenderSurvey(surveyID) {
     $.get(`/api/survey/${surveyID}`, function(data) {
-      console.log(data.formData.survey.name);
-      let name = data.formData.survey.name;
-      $('.selection-1').append(`<h3>${name}</h3>`)
-    })
-
-  }
+        if (surveyID === null) {
+          console.log('There is no survey')
+        } else {
+          console.log(data.formData.survey);
+          let name = data.formData.survey.name;
+          let selection1 = data.formData.survey.selections[0].text;
+          let selection2 = data.formData.survey.selections[1].text;
+          let selection3 = data.formData.survey.selections[2].text;
+          let selection4 = data.formData.survey.selections[3].text;
+          $('.question').append(`
+            <h2 class="form-name">${name}</h2>`)
+          $('.selection-1').append(`<div class='selection-container'>
+            <input
+              type='radio'
+              name='answer'
+              class='radio'
+              value='selection1'>
+            </input>
+            <h3 class='selection'>${selection1}</h3></div>`)
+          $('.selection-2').append(`<div class='selection-container'>
+            <input
+              type='radio'
+              name='answer'
+              class='radio'
+              value='selection2'>
+            </input>
+            <h3 class='selection'>${selection2}</h3></div>`)
+          $('.selection-3').append(`<div class='selection-container'>
+            <input
+              type='radio'
+              name='answer'
+              class='radio'
+              value='selection3'>
+            </input>
+            <h3 class='selection'>${selection3}</h3></div>`)
+          $('.selection-4').append(`<div class='selection-container'>
+            <input
+              type='radio'
+              name='answer'
+              class='radio'
+              value='selection4'>
+            </input>
+            <h3 class='selection'>${selection4}</h3></div>`)
+        }
+      })
+    }
 
   function getParameterByName(name, url) {
     if (!url) {
@@ -26,13 +63,4 @@ $(document).ready(function() {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }
-
-  function renderSurvey(surveyID) {
-    if (surveyID === null) {
-      console.log('There is no survey')
-    } else {
-    $('.question').append(`<h3>${surveyID}</h3>`)
-    $('.selection-1').append(`<h3>${name}</h3>`)
-    }
   }
--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ const environment = process.env.NODE_ENV || 'development';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 app.set('port', process.env.PORT || 3000);
